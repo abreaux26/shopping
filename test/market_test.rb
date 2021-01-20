30c453b4a0cf993eca768e3e6344d8622b84463b
Angel Breaux <abreaux@detaso.com>
2021-01-20 10:25:50 -0600

    Add add vendor

--- test/market_test.rb
+++ test/market_test.rb
@@ -7,6 +7,10 @@ require './lib/market'
 class MarketTest < Minitest::Test
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = mock
+    @item1 = mock
+    @item2 = mock
+    @vendor1.stubs(:stock).returns({@item1 => 35}, {@item2 => 7})
   end
 
   def test_it_exists
@@ -16,5 +20,10 @@ class MarketTest < Minitest::Test
   def test_it_has_readable_attributes
     assert_equal "South Pearl Street Farmers Market", @market.name
     assert_equal [], @market.vendors
+  end
+
+  def test_add_vendor
+    @market.add_vendor(@vendor1)
+    assert_equal [@vendor1], @market.vendors
   end
 end
