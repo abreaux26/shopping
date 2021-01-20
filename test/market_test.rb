fb4e71027e210deb4385f91830aabbfecc16bca7
Angel Breaux <abreaux@detaso.com>
2021-01-20 10:29:36 -0600

    Create vendor names

--- test/market_test.rb
+++ test/market_test.rb
@@ -8,6 +8,8 @@ class MarketTest < Minitest::Test
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
     @vendor1 = mock
+    @vendor2 = mock
+    @vendor3 = mock
     @item1 = mock
     @item2 = mock
     @vendor1.stubs(:stock).returns({@item1 => 35}, {@item2 => 7})
@@ -25,5 +27,17 @@ class MarketTest < Minitest::Test
   def test_add_vendor
     @market.add_vendor(@vendor1)
     assert_equal [@vendor1], @market.vendors
+  end
+
+  def test_market_vendor_names
+    @vendor1.stubs(:name).returns("Rocky Mountain Fresh")
+    @vendor2.stubs(:name).returns("Ba-Nom-a-Nom")
+    @vendor3.stubs(:name).returns("Palisade Peach Shack")
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+    assert_equal expected, @market.vendor_names
   end
 end
